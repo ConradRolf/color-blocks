6a8a952ac6ddc8d393924788e44d3713b104f532
Conrad Rolf <williamconradrolf@gmail.com>
2023-05-04 18:44:34 -0500

    added functions for clicking buttons to add different colored blocks onto the DoM. created functions for increasing the count of the different colored blocks on the DoM. Created a function for removing clicked on blocks.

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
+$(document).ready(onReady);
 console.log('🟥 🟦 🟩 🟨');
+
+function onReady(){
+    console.log('Get some colors going!')
+
+    $('#red-btn').on('click', redBlock);
+
+    $('#blue-btn').on('click', blueBlock);
+
+    $('#green-btn').on('click', greenBlock);
+
+    $('#yellow-btn').on('click', yellowBlock);
+
+    // $('.allBlocks').on('click', '.block', eatBlock);
+
+    $('#red-btn').on('click', rCount)
+
+    $('#blue-btn').on('click', bCount)
+
+    $('#green-btn').on('click', gCount)
+
+    $('#yellow-btn').on('click', yCount)
+}
+
+function rCount(){
+    $('#red-count').html(function(i, val){
+        return +val+1
+    })
+}
+
+function bCount(){
+    $('#blue-count').html(function(i, val){
+        return +val+1
+    })
+}
+
+function gCount(){
+    $('#green-count').html(function(i, val){
+        return +val+1
+    })
+}
+
+function yCount(){
+    $('#yellow-count').html(function(i, val){
+        return +val+1
+    })
+}
+
+function redBlock(){
+    $('.allBlocks').append(`<p class="block">🟥</p>`)
+}
+
+function blueBlock(){
+    $('.allBlocks').append(`<p class="block">🟦</p>`)
+}
+
+function greenBlock(){
+    $('.allBlocks').append(`<p class="block">🟩</p>`)
+}
+
+function yellowBlock(){
+    $('.allBlocks').append(`<p class="block">🟨</p>`)
+}
+
+// function eatBlock(){
+//     $(this).remove();
+// }
+
